d78738b8cc8622d576620789e6c70d8143677d44
Илья <git@github.com:Ilyaklyukin152/helloworld.git>
2025-02-09 17:52:44 +0300

    Homework done

---
+++ src/Book.java
@@ -0,0 +1,33 @@
+public class Book {
+    private String title;
+    private Author author;
+    private int publishDate;
+
+    public Book(String title, Author author, int publishDate) {
+        this.title = title;
+        this.author = author;
+        this.publishDate = publishDate;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public Author getAuthor() {
+        return author;
+    }
+
+    public int getPublishDate() {
+        return publishDate;
+    }
+
+    public void setPublishDate(int publishDate) {
+        this.publishDate = publishDate;
+    }
+
+    public String toString() {
+        return "Название книги: " + title +
+                ", Автор: " + author +
+                ", Дата публикации: " + publishDate;
+    }
+}
